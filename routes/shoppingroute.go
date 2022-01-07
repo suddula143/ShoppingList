2079de43e9208f8d1a4ed4a31fbc74da859832fa
suddula143 <suddulahari143@gmail.com>
2022-01-07 13:56:18 +0530

    chages

---
+++ routes/shoppingroute.go
@@ -0,0 +1,10 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func ShopGroup(r *gin.Engine) {
+	shopGroup := r.Group("/user")
+	{
+		shopGroup.POST("shoppinglist", shopController.AddShop)
+	}
+}
