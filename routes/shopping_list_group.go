d9c32adfc8fa6aea7c5eb063cbd4404b80abe191
suddula143 <suddulahari143@gmail.com>
2022-01-09 20:56:39 +0530

    added to lint

--- routes/shopping_list_group.go
+++ routes/shopping_list_group.go
@@ -3,6 +3,7 @@ package routes
 import "github.com/gin-gonic/gin"
 
 //ShoppingListGroup gives the path to connect to db
+
 func ShoppingListGroup(r *gin.Engine) {
 	shoppingListGroup := r.Group("/shopping-list")
 	{
